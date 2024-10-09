10a4d19fea79bc7479f4412a6420d347d382cbc6
Manali Raut <raut.ma@northeastern.edu>
2024-10-09 10:11:32 -0400

    adding new tf files

---
+++ src/terraform/main.tf
@@ -0,0 +1,62 @@
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnets_cidrs)  
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = element(var.public_subnets_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnets_cidrs)  
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = element(var.private_subnets_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "my_gateway" {
+ vpc_id = aws_vpc.my_vpc.id
+ 
+ tags = {
+   Name = var.internet_gateway_name
+ }
+}
+
+resource "aws_route_table" "public_routing_table" {
+ vpc_id = aws_vpc.my_vpc.id
+ 
+ route {
+   cidr_block = var.public_cidr_routing_table
+   gateway_id = aws_internet_gateway.my_gateway.id
+ }
+ 
+ tags = {
+   Name = var.public_routing_table_name
+ }
+}
+
+resource "aws_route_table_association" "public_association" {
+  count          = length(aws_subnet.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_routing_table.id
+}
+
+resource "aws_route_table" "private_routing_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = var.private_routing_table_name
+    }
+}
+
+resource "aws_route_table_association" "private_association" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_routing_table.id
+}
---
+++ src/terraform/vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_range
+
+  tags = {
+    Name = var.vpc_name
+  }
+  "
+}
