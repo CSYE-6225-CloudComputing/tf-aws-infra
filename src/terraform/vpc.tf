44443b7cad36a334bb45730c139654169fdbe736
Manali Raut <raut.ma@northeastern.edu>
2024-10-09 10:02:13 -0400

    adding tf files

--- src/terraform/vpc.tf
+++ src/terraform/vpc.tf
@@ -1,7 +1,8 @@
-resource "aws_vpc1 "my_vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_range
-
-  tags = {
+  
+  # Introduce a syntax error by adding an extra quote or wrong syntax
+  tags = { 
     Name = var.vpc_name
-  }
+  }"
 }
