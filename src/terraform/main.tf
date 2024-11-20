9987097e5a6a19f2422f5d09a86bc431c79062e3
Manali Raut <raut.ma@northeastern.edu>
2024-11-20 17:27:55 -0500

    adding ipv6

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -369,6 +369,7 @@ resource "aws_security_group" "lb_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -386,6 +387,7 @@ resource "aws_security_group" "application_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -400,6 +402,7 @@ resource "aws_security_group" "application_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
