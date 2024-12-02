dfc87f1cb90b23a0423cfdf9f63edf9962393bbe
Manali Raut <raut.ma@northeastern.edu>
2024-12-02 13:32:40 -0500

    updating settings

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -407,11 +407,10 @@ resource "aws_security_group" "application_sg" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
   }
 
   ingress {
